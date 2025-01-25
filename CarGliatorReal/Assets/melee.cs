dc6f7aa582e3f28730f73a95d9a3f848a622c5cb
lahavior <128811783+lahavior@users.noreply.github.com>
2025-01-25 16:20:57 +0200

    combat kinda (Lahav scene)
    the enemy should have an enemy tag and the attack force should be about 10

---
+++ CarGliatorReal/Assets/melee.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class melee : MonoBehaviour
+{
+    [SerializeField] private float attackForce;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            if (other.gameObject.CompareTag("enemy"))
+            {
+                // Get the direction the object is facing (forward vector)
+                Vector3 dir = transform.forward;
+
+                // Apply the knockback force to the enemy's rigidbody
+                other.attachedRigidbody.AddForce(dir.normalized * attackForce, ForceMode.Impulse);
+            }
+        }
+    }
+
+}
