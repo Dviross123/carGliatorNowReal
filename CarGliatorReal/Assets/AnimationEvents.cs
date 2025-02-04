f9456d7d7e3ca74e3f2a0797da860739c2f3619c
lahavior <128811783+lahavior@users.noreply.github.com>
2025-02-04 15:16:05 +0200

    particles yes

--- CarGliatorReal/Assets/AnimationEvents.cs
+++ CarGliatorReal/Assets/AnimationEvents.cs
@@ -1,13 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
+using Unity.Netcode;
 using UnityEngine;
 
-public class AnimationEvents : MonoBehaviour
+
+public class AnimationEvents : NetworkBehaviour
 {
+    [SerializeField] GameObject dust;
     void SetBoolFalse(string boolNaME)
     {
         Animator anim= GetComponent<Animator>();
         anim.SetBool(boolNaME, false);
         GetComponent<melee>().canDamage = true;
+    }
+
+    [ServerRpc]
+    void MakeParticlesServerRpc()
+    {
+        GameObject spawnDustPs =  Instantiate(dust, transform.position, quaternion.identity);
+        spawnDustPs.GetComponent<NetworkObject>().Spawn(true);
+        
     }
 }
